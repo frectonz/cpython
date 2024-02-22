e5cf32684661aff32aa865f4a1c57483933825a3
frectonz <fraol0912@gmail.com>
2024-02-22 15:25:38 +0300

    simplify `mutate.sh`

--- mutate.sh
+++ mutate.sh
@@ -7,46 +7,20 @@ set -e
 make regen-pegen
 
 replace_patterns() {
-    find "$1" -type f -name '*.py' -exec sed -i "s/$2/$3/g" {} +
+    find "$1" -type f -name '*.py' -exec sed -i "s/\b$2\b/$3/g" {} +
 }
 
 # Change 'todo' to 'todo_'
 replace_patterns "Lib" "todo" "todo_"
 
-# Replace 'return' with 'bababooey' in Lib, Programs, Tools, and Modules
 for dir in "Lib" "Programs" "Tools" "Modules"; do
     replace_patterns "$dir" "return" "bababooey"
-done
-
-# Replace 'def' with 'doer' in Lib, Programs, Tools, and Modules
-for dir in "Lib" "Programs" "Tools" "Modules"; do
-    replace_patterns "$dir" "def " "doer "
-done
-
-# Replace 'if' with 'noicely' in Lib, Programs, Tools, and Modules
-for dir in "Lib" "Programs" "Tools" "Modules"; do
-    replace_patterns "$dir" "\bif " "noicely "
-done
-
-# Replace 'class' with 'room' in Lib, Programs, Tools, and Modules
-for dir in "Lib" "Programs" "Tools" "Modules"; do
-    replace_patterns "$dir" "\bclass " "room "
-done
-
-# Replace 'try' with 'fuck_around' in Lib, Programs, Tools, and Modules
-for dir in "Lib" "Programs" "Tools" "Modules"; do
-    replace_patterns "$dir" "\btry:" "fuck_around:"
-done
-
-# Replace 'except' with 'find_out' in Lib, Programs, Tools, and Modules
-for dir in "Lib" "Programs" "Tools" "Modules"; do
-    replace_patterns "$dir" "\bexcept " "find_out "
-    replace_patterns "$dir" "\bexcept:" "find_out:"
-done
-
-# Replace 'elif' with 'else_if' in Lib, Programs, Tools, and Modules
-for dir in "Lib" "Programs" "Tools" "Modules"; do
-    replace_patterns "$dir" "\belif " "else_if "
+    replace_patterns "$dir" "def"    "doer"
+    replace_patterns "$dir" "if"     "noicely"
+    replace_patterns "$dir" "class"  "room"
+    replace_patterns "$dir" "try"    "fuck_around"
+    replace_patterns "$dir" "except" "find_out"
+    replace_patterns "$dir" "elif"   "else_if"
 done
 
 # Make the mutated Python
